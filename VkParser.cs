0074eec0842f4063d1e3cc890e84faf9edfca8b0
Murk <32967078+nydopustimname@users.noreply.github.com>
2020-10-28 13:26:00 +0300

    Update VkParser.cs

--- VkParser.cs
+++ VkParser.cs
@@ -1,18 +1,6 @@
 ﻿using AngleSharp.Html.Dom;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VkNet;
-using VkNet.Enums.Filters;
-using VkNet.Exception;
-using VkNet.Model;
-using VkNet.Model.RequestParams;
-using VkNet.Utils;
-using AngleSharp.Html.Parser;
-using System.Threading;
-using System.Diagnostics.Eventing.Reader;
+
 using System.Net;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -21,45 +9,6 @@ namespace AlfaTask2
 {
    public class VkParser
     {
-       
-        private readonly ulong AppId = 7642894;
-        private readonly VkApi Vkapi;
-        ApiAuthParams ApiParams;
-
-        public event OnCompleted OnCompleted;
-        public event NewMessage NewMessage;
-
-        public VkParser()
-        {
-            Vkapi = new VkApi();
-            ApiParams = new ApiAuthParams();
-        }
-
-        UserModel user;
-        protected UserModel userModel
-        {
-            get => user;
-            set
-            {
-                user = value;
-                
-            }
-        }
-
-        public bool VkAuth(string login, string password)
-        {
-            ApiParams.ApplicationId = AppId;
-            ApiParams.Login = login;
-            ApiParams.Password = password;
-            ApiParams.Settings = Settings.All;
-
-             Vkapi.AuthorizeAsync(ApiParams);
-
-            if (Vkapi.IsAuthorized) NewMessage?.Invoke("Вы авторизировались");
-            return Vkapi.IsAuthorized;
-        }
-        
-       
         public string ParseAsync(string url)
         {
             WebClient client = new WebClient();
