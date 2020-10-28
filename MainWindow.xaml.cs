bfd04bb960e4a2bf54d26ae5764dacb3e90614ee
nudopustimname <tanya.kavretskaya@mail.ru>
2020-10-28 13:41:39 +0300

    unused code deleted

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,9 @@
-﻿using AlfaTask2.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AlfaTask2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+  
     public partial class MainWindow : Window
     {
         private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
@@ -27,12 +11,12 @@ namespace AlfaTask2
         public MainWindow()
         {
             InitializeComponent();
-            Loaded += MainWindow_Loaded;
         }
 
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+       private void UpdButton_Click(object s, RoutedEventArgs e)
         {
-            DataContext = new UserViewModel();
+            textBox1.Text = "";
+            textBox2.Text = "";
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -44,10 +28,5 @@ namespace AlfaTask2
         }
 
        
-
-        private void Text_Changed(object sender, RoutedEventArgs e)
-        {
-            
-        }
     }
 }
--- VkParser.cs
+++ VkParser.cs
@@ -1,72 +1,26 @@
-﻿using AngleSharp.Html.Dom;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VkNet;
-using VkNet.Enums.Filters;
-using VkNet.Exception;
-using VkNet.Model;
-using VkNet.Model.RequestParams;
-using VkNet.Utils;
-using AngleSharp.Html.Parser;
-using System.Threading;
-using System.Diagnostics.Eventing.Reader;
+﻿using System;
 using System.Net;
 using System.IO;
 using System.Text.RegularExpressions;
 
 namespace AlfaTask2
 {
-   public class VkParser
+    public class VkParser
     {
-       
-        private readonly ulong AppId = 7642894;
-        private readonly VkApi Vkapi;
-        ApiAuthParams ApiParams;
-
-        public event OnCompleted OnCompleted;
-        public event NewMessage NewMessage;
-
-        public VkParser()
-        {
-            Vkapi = new VkApi();
-            ApiParams = new ApiAuthParams();
-        }
-
-        UserModel user;
-        protected UserModel userModel
-        {
-            get => user;
-            set
-            {
-                user = value;
-                
-            }
-        }
-
-        public bool VkAuth(string login, string password)
-        {
-            ApiParams.ApplicationId = AppId;
-            ApiParams.Login = login;
-            ApiParams.Password = password;
-            ApiParams.Settings = Settings.All;
-
-             Vkapi.AuthorizeAsync(ApiParams);
-
-            if (Vkapi.IsAuthorized) NewMessage?.Invoke("Вы авторизировались");
-            return Vkapi.IsAuthorized;
-        }
-        
        
         public string ParseAsync(string url)
         {
             WebClient client = new WebClient();
             client.Encoding = System.Text.Encoding.UTF8;
-            Stream stream = client.OpenRead(url);
-            if (stream == null)
+            Stream stream;
+            try
+            {
+                stream = client.OpenRead(url);
+            }
+            catch (Exception e)
+            {
                 return "Invalid link";
+            }
             StreamReader sr = new StreamReader(stream);
             string newLine;
 
